21b723fd82843c95682a064bfb52d1ac759be1bc
Blessing600 <69908280+Blessing600@users.noreply.github.com>
2024-11-06 12:57:36 +0100

    Resolve issue with page switching for Pending Verification

--- Components/Icons.tsx
+++ Components/Icons.tsx
@@ -900,7 +900,7 @@ export const AccountNotificationIcon = () => {
     </svg>
   );
 };
-export const HistoryArrowIcon = ({style}) => {
+export const HistoryArrowIcon = () => {
   return (
     <svg width="12" height="8" viewBox="0 0 12 8" fill="none" xmlns="http://www.w3.org/2000/svg">
       <g clip-path="url(#clip0_1_8236)">

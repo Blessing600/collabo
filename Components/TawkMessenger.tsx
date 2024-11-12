555cbf8079edda6cb03e9570441c9371d0ebfae7
Blessing600 <69908280+Blessing600@users.noreply.github.com>
2024-11-12 07:01:21 +0100

    Fix referral banner UI on rent page for mobile adjust Tawk chat icon positioning on mobile

--- Components/TawkMessenger.tsx
+++ Components/TawkMessenger.tsx
@@ -1,13 +1,30 @@
 "use client";
 import { useRef } from "react";
-import TawkMessengerReact, { TawkMessenger } from "@tawk.to/tawk-messenger-react";
+import TawkMessengerReact, {
+  TawkMessenger,
+} from "@tawk.to/tawk-messenger-react";
 
 const TawkMessengerComponent = () => {
+  const customStyle = {
+    visibility: {
+      mobile: {
+        xOffset: 10,
+        yOffset: 70,
+        position: "tr"
+      },
+    },
+  };
+
   const tawkMessengerRef = useRef<TawkMessenger>(null);
 
   return (
     <div>
-      <TawkMessengerReact propertyId="655381bacec6a912820fc8a3" widgetId="1hf735gcu" ref={tawkMessengerRef} />
+      <TawkMessengerReact
+      customStyle={ customStyle }
+        propertyId="655381bacec6a912820fc8a3"
+        widgetId="1hf735gcu"
+        ref={tawkMessengerRef}
+      />
     </div>
   );
 };
